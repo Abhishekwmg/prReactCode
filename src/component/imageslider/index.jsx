b8c031175a581ff98097c0630b983b1d8f234901
Abhishekwmg <ak89471@gmail.com>
2025-12-09 23:51:04 +0530

    Image Slider Impl

--- src/component/imageslider/index.jsx
+++ src/component/imageslider/index.jsx
@@ -4,6 +4,8 @@ import './style.css'
 export default function ImageSlider() {
 
     const [image, setImages] = useState([]);
+    const [currImgSlide, setCurrImgSlide] = useState(0);
+    console.log(image);
 
     async function fetchImage() {
         const response = await fetch("https://picsum.photos/v2/list?page=2&limit=10");
@@ -18,21 +20,26 @@ export default function ImageSlider() {
 
 
 
-    function handlePrev() { }
-    function handleNext() { }
+    function handlePrev() {
+        setCurrImgSlide(prevCurrSlide => prevCurrSlide - 1);
+    }
+    function handleNext() {
+        setCurrImgSlide(prevCurrSlide => prevCurrSlide + 1)
+    }
+
     return <div className='container'>
         <button className='left-arrow' onClick={handlePrev}>Prev</button>
         {image && image.length ?
             image.map((imageItem) => {
-                return <img key={imageItem.download_url} src={imageItem.download_url} />
+                return <img key={imageItem.download_url} src={image[currImgSlide].download_url} />
             }) : null
         }
         <button className='right-arrow' onClick={handleNext}>Next</button>
-        {
+        {/* {
             [1, 2, 3, 4, 5].map((_, index) => {
                 return <span key={index} className='current-indicator'>*</span>
             })
-        }
+        } */}
     </div>
 }
 
